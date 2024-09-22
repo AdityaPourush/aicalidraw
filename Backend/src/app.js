361c6ba5e640436e6177f954e45da1cbb94b7e56
Aditya Pourush <adityapourush11@outlook.com>
2024-09-22 12:16:24 +0530

    cors updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,12 @@ const imageRoutes = require('./routes/imageRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
