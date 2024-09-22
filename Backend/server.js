f300bb36b407c6087fa5e28a9c250da0ba56ddfb
Aditya Pourush <adityapourush11@outlook.com>
2024-09-22 12:47:39 +0530

    backend gets for checks

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => res.send(`Server running on port ${PORT}`));
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- Backend/src/routes/imageRoutes.js
+++ Backend/src/routes/imageRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const imageController = require('../controllers/imageController');
 
+router.get('/', (req,res) => {
+    res.send("On route /api")
+})
 router.post('/calculate', imageController.processImage);
 
 module.exports = router;
